47df1c6dfd1a61750c76ec1055f64a1d33825edb
niloccolinus <niloccolin@protonmail.com>
2024-12-13 13:53:48 +0100

    added AR test

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,41 @@
-console.log("hello world");
+import * as THREE from "../../node_modules/three/build/three.module.js";
+import { ARButton } from "../../node_modules/three/examples/jsm/webxr/ARButton.js";
+
+// test code from https://www.npmjs.com/package/three
+
+const width = window.innerWidth,
+  height = window.innerHeight;
+
+// init
+
+const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
+camera.position.z = 1;
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+const material = new THREE.MeshNormalMaterial();
+
+const cube = new THREE.Mesh(geometry, material);
+cube.position.set(0, 0, -2);
+scene.add(cube);
+
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+  alpha: true,
+});
+
+renderer.setSize(width, height);
+renderer.xr.enabled = true;
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+document.body.appendChild(ARButton.createButton(renderer));
+
+// animation
+
+function animate(time) {
+  cube.rotation.x = time / 2000;
+  cube.rotation.y = time / 1000;
+
+  renderer.render(scene, camera);
+}
