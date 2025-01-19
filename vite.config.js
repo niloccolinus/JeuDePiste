d9e9eec8f849af142bb8b4ccfdcaa9e16ea0b74a
niloccolinus <niloccolin@protonmail.com>
2025-01-19 11:11:27 +0100

    changed vite config & added home icon

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
 import { defineConfig } from "vite";
 import path from "path";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: "/", // Chemin de base pour votre application
-  build: {
-    outDir: "dist", // Dossier de sortie pour Netlify
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"), // Optionnel : Alias pour vos imports
+    base: "/", // Chemin de base pour votre application
+    build: {
+        outDir: "dist",
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                step1: resolve(__dirname, "step1.html"),
+                step2: resolve(__dirname, "step2.html"),
+                step3: resolve(__dirname, "step3.html"),
+                step4: resolve(__dirname, "step4.html"),
+            },
+        },
     },
-  },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"), // Optionnel : Alias pour vos imports
+        },
+    },
 });
